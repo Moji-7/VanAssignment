bdfabc11eb1ced0721ce37e1c30131adcd58733d
Moji <mirzaei_7@yahoo.com>
2022-04-01 11:19:55 +0430

    Update IMoneyTransferService.cs

--- TransferApi/Services/IMoneyTransferService.cs
+++ TransferApi/Services/IMoneyTransferService.cs
@@ -11,15 +11,7 @@ namespace TransferApi.Services
 
 
 
-        //List<MoneyTransfer> getPendingMoneyTransfers();
-
-        //  List<MoneyTransfer> getDisputedTransfers() throws Exception;
-
-        // void inquiry(String ssn, MoneyTransfer transaction) throws Exception;
-
-        // void transfer(MoneyTransfer transaction, String ssn, MoneyTransferIntegrationService integrationService) throws Exception;
-
-        ///<exception cref="AccountNotFoundException">is thrown if the profile does not exists</exception>
+        ///<exception cref="AccountNotFoundException">is thrown if the account is not accessible</exception>
         Task<TransferDto> CreateTransfer(Transfer transfer);
         Task<TransferDto> GetTransferDetails(Guid ownerId);
         Task<IEnumerable<TransferDto>> GetTransfers(TransferSearchDto transferSearchDto);
